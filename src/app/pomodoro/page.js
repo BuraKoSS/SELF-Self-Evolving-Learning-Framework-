8ef1be4561cf4186bb71e933d42aef6241c05f5c
talhaozkayi <171152706+talhaozkayi@users.noreply.github.com>
2025-12-02 11:21:12 +0300

    Add files via upload

---
+++ src/app/pomodoro/page.js
@@ -0,0 +1,10 @@
+import PomodoroTimer from "../../components/PomodoroTimer";
+import "./style.css";
+
+export default function PomodoroPage() {
+    return (
+        <div className="wrapper">
+            <PomodoroTimer />
+        </div>
+    );
+}
